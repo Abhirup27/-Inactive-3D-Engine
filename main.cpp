af069362465674fba7ee05078c47432ae65778e6
Abhirup27 <55529237+Abhirup27@users.noreply.github.com>
2021-05-10 19:02:58 +0530

    Added IMGUI
    added a working GUI and changed some RGB updating calculations in while render loop.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,37 @@
 
+
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+#include <GL/gl3w.h>            // Initialize with gl3wInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+#include <GL/glew.h>            // Initialize with glewInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+#include <glad/glad.h>          // Initialize with gladLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+#include <glad/gl.h>            // Initialize with gladLoadGL(...) or gladLoaderLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/Binding.h>  // Initialize with glbinding::Binding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/glbinding.h>// Initialize with glbinding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#endif
+
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
+#include<SHADER_S.h>
+
 #include<iostream>
-#include<glad/glad.h>
+
 #include<GLFW/glfw3.h>
 #include<math.h>
 
-
-#include<SHADER_S.h>
 /*
 const char *vertexShaderSource =
 #include "shaders/test.vs"
@@ -14,6 +40,9 @@ const char *fragmentShaderSource =
 #include "shaders/Tfrag.fs"
 ;
 */
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     {
         std::cout<<glfwGetCurrentContext()<<std::endl;
@@ -79,8 +108,9 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4.6);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
+    const char* glsl_version = "#version 130";
     GLFWwindow* window = glfwCreateWindow(1920, 1080, "Enlist 3D", NULL,NULL);
+
     if(window == NULL)
     {
 
@@ -89,6 +119,38 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    #if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+    bool err = gl3wInit() != 0;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+    bool err = gladLoadGL() == 0;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+    bool err = gladLoadGL(glfwGetProcAddress) == 0; // glad2 recommend using the windowing library loader instead of the (optionally) bundled one.
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+    bool err = false;
+    glbinding::Binding::initialize();
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+    bool err = false;
+    glbinding::initialize([](const char* name) { return (glbinding::ProcAddress)glfwGetProcAddress(name); });
+#else
+    bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
+#endif
+    if (err)
+    {
+        fprintf(stderr, "Failed to initialize OpenGL loader!\n");
+        return 1;
+    }
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+     // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
 
@@ -97,6 +159,11 @@ int main()
     }
     glViewport(0, 0, 1920, 1080);
 
+     // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    io.Fonts->AddFontFromFileTTF("OpenSans-Regular.ttf", 38.0f);
 
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
@@ -149,11 +216,21 @@ int main()
     //glDeleteShader(fragmentShader);
     glBindVertexArray(0);
 
+
+    // Our state
+    bool show_demo_window = true;
+    bool show_another_window = true;
+    bool use_sin_func =false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+
+
     //Call Back functions
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); //after the window is created, SetFramebufferSizeCallback checks if the window size is changed and calls frame_size_callback inside its definition providing width & height as parameters
     while(!glfwWindowShouldClose(window))
     {
-        rvalue += 0.001f, gvalue += 0.0005f, bvalue += 0.0001f;
+
+        //rvalue += 0.001f, gvalue += 0.0005f, bvalue += 0.0001f;
         //check for inputs
         processInput(window);
 
@@ -162,19 +239,21 @@ int main()
         glClearColor(rvalue,gvalue,bvalue,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-
+        float blueValue= 1.0f;
         float timeValue = glfwGetTime();
-        float greenValue =(sin(timeValue)/ 2.0f) +0.5f;
-        float redValue = (sin(timeValue)/ 2.5f) *1.86f;
+        float greenValue = 0.2f;  //(sin(timeValue)/ 2.0f) +0.5f;
+        float redValue = 0.5f; //(sin(timeValue)/ 2.5f) *1.86f;
         //int vertexColorLocation = glGetUniformLocation(shaderProgram, "GreenVal");
         //int vertexColorLocation2 = glGetUniformLocation(shaderProgram, "RedVal");
 
+        if(use_sin_func== true)
+        {
+        rvalue = (sin(timeValue)/ 2.0f) +0.7f;
+        gvalue = (sin(timeValue)/ 2.09f) +0.5f;
+        bvalue = (sin(timeValue)/ 2.3f) +0.5f;
+        }
 
         //ALWAYS DRAW AFTER UPDATING THE BUFFER!
-        ourShader.use();
-        ourShader.setFloat("GreenVal",greenValue);
-        ourShader.setFloat("RedVal", redValue);
-
         //glUseProgram(shaderProgram);
         //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         //glUniform1f(vertexColorLocation, greenValue);
@@ -185,9 +264,69 @@ int main()
          glBindVertexArray(0);
         //check and call for events and swap the front and back buffer
         glfwPollEvents();
+
+        // Start the Dear ImGui frame
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("WINDOW SETTINGS");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("TEST WINDOW.",48.0f);               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+            ImGui::Checkbox("Use sine function ", &use_sin_func);
+            ImGui::SliderFloat("Window Red Color", (float*)&rvalue, 0.0f, 1.0f);
+            ImGui::SliderFloat("Window Green Color", (float*)&gvalue, 0.0f, 1.0f);
+            ImGui::SliderFloat("Window Blue Color", (float*)&bvalue, 0.0f, 1.0f);
+            ImGui::SliderFloat3("Rectangle RGB", (float*)&clear_color, 0.0f, 1.0f);
+                       // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+        ourShader.use();
+        ourShader.setFloat("GreenVal",clear_color.y);
+        ourShader.setFloat("RedVal", clear_color.x);
+        ourShader.setFloat("BluVal", clear_color.z);
+        // Rendering
+        ImGui::Render();
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
 
     }
+        ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
 //    glDeleteProgram(shaderProgram);
--- shaders/Tfrag.fs
+++ shaders/Tfrag.fs
@@ -4,6 +4,7 @@ out vec4 FragColor;
 
 uniform float GreenVal;
 uniform float RedVal;
+uniform float BluVal;
 in vec3 ourColor;
 
 void main()
@@ -12,6 +13,6 @@ void main()
 //FragColor =vertexColor;
 //FragColor = ourColor;
 
-FragColor = vec4(ourColor.x * RedVal,ourColor.y * GreenVal,ourColor.z, 1.0f);
+FragColor = vec4(ourColor.x * RedVal,ourColor.y *GreenVal ,ourColor.z *BluVal, 1.0f);
 }
 
